d96d34b8608f016dc247e5d83913a894eafe97bf
jasenfinch <jsf9@aber.ac.uk>
2017-06-06 00:47:28 +0100

    added availableDataSets function

---
+++ R/availableDataSets.R
@@ -0,0 +1,18 @@
+#' availableDataSets
+#' @description Displays file paths for available data sets.
+#' @export
+#' @examples 
+#' availableDataSets()
+#' 
+#' ## Can be run to access individual file paths
+#' list.files(system.file('DataSets/FIE-HRMS/BdistachyonEcotypes',package = 'metaboData'),full.names = TRUE)
+
+availableDataSets <- function(){
+   dirs <- list.dirs(system.file('DataSets',package = 'metaboData'),recursive = F)
+   dataSets <- lapply(dirs,function(x){
+       tech <- strsplit(x,'/')
+       cat('\n',tech[[1]][length(tech[[1]])])
+       dataSets <- list.dirs(x,recursive = F)
+       cat('\n',dataSets,'\n\n')
+   })
+}
