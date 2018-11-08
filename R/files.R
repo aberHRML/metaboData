a2ad9c9e40904dbc280e2fd135d487416dccdd53
jasenfinch <jsf9@aber.ac.uk>
2018-11-08 11:19:09 +0000

    added functions for data set retrieval. changed version to 0.2.0

---
+++ R/files.R
@@ -0,0 +1,32 @@
+#' @importFrom stringr str_c
+#' @export
+
+files <- function(technique,dataSet){
+    list.files(system.file(str_c('DataSets',technique,dataSet,sep = '/'),package = 'metaboData'),full.names = T)
+}
+
+#' @importFrom magrittr %>%
+#' @importFrom stringr str_split
+#' @importFrom purrr map_chr
+#' @export
+
+techniques <- function(){
+    list.dirs(system.file('DataSets',package = 'metaboData'),recursive = F) %>%
+        map_chr(~{
+            str_split(.,'/') %>%
+                .[[1]] %>%
+                .[length(.)]    
+        })
+        
+}
+
+#' @export
+
+dataSets <- function(technique){
+    list.dirs(system.file(str_c('DataSets',technique,sep = '/'),package = 'metaboData'),recursive = F) %>%
+        map_chr(~{
+            str_split(.,'/') %>%
+                .[[1]] %>%
+                .[length(.)]  
+        })
+}
